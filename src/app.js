d40f5e104ead2b31131b5c27986375e359c82554
Aung Myat <aungmyat@blueplanet.com.mm>
2021-12-14 11:38:02 +0630

    backend test1

--- src/app.js
+++ src/app.js
@@ -4,4 +4,4 @@ const chartRouter = require('./app/routers/chartRouter')
 app.use(express.urlencoded({extended : true}))
 app.use(express.json())
 app.use('/api/chart' , chartRouter)
-module.exports = {app}
+ module.exports = {app}
